05192ffd6f01e9bb4bacf82f4ff0e97b8c98fa87
25rae25 <rae0024@naver.com>
2020-06-08 14:49:17 +0900

    0608 수업

---
+++ js/38.ready.js
@@ -0,0 +1,14 @@
+
+		console.log( $(".wrapper").html() );
+
+		console.log( $("#LastName, .intro") );
+		console.log( $("intro") );
+		console.log( $(".intro, #LastName") );
+		console.log( $("#LastName").find(".intro") );
+		
+		
+		
+		$(document).ready(function(){
+
+		});
+
